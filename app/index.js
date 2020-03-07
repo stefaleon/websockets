853d0650641d97cf0cf40e840c8609c6d8445c0a
stefaleon <stefaleon@gmail.com>
2020-03-07 21:25:49 +0200

    refactor basic server in order to use with socket.io

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
+const http = require('http');
 const express = require('express');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 
@@ -10,4 +12,4 @@ app.get('/', (req, res) => {
   res.json({ main: 'ok' });
 });
 
-app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
+server.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
