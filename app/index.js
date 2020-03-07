abb56ba684ae3961f06c57c5d7b79d3a8452ab65
stefaleon <stefaleon@gmail.com>
2020-03-07 21:35:19 +0200

    listen for socket connections

--- app/index.js
+++ app/index.js
@@ -1,15 +1,19 @@
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ main: 'ok' });
 });
+
+io.on('connection', () => console.log('new websocket connection'));
 
 server.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
