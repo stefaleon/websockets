553fa59574ef258ca99665bd1cac61810f7c6780
stefaleon <stefaleon@gmail.com>
2020-03-08 10:29:23 +0200

    emit event from the client

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,3 +2,8 @@ const socket = io();
 socket.on('countUpdated', count =>
   console.log('countUpdated, count is:', count)
 );
+
+document.querySelector('#increment').addEventListener('click', () => {
+  console.log('increment button clocked');
+  socket.emit('countIncremented');
+});
