e54856ad65c248d3e8779013e4b080187a0b1006
stefaleon <stefaleon@gmail.com>
2020-03-08 11:06:46 +0200

    clients emit messages from a form

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,3 +4,11 @@ socket.on('welcomeSent', msg => {
   console.log(msg);
   document.querySelector('#message').innerHTML = msg;
 });
+
+document.querySelector('#message-form').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const clientMessage = document.querySelector('input').value;
+
+  socket.emit('clientSentMessage', clientMessage);
+});
